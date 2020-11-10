3bd39d7807b4e3ddea0bc485daf578f511426d41
kluzko <kluzniakkuba@gmail.com>
2020-11-10 22:18:03 +0100

    connected to mongoDB & add to termianl outputs colors

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-// Route files
-
-const albums = require("./routes/albums");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to mongo database
+connectDB();
+
+// Route files
+const albums = require("./routes/albums");
+
 const app = express();
 
 // Dev logging middleware
-
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
@@ -20,8 +24,15 @@ app.use("/api/v1/albums", albums);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
-    `App listening on port ${PORT} and mode is ${process.env.NODE_ENV}`
+    `App listening on port ${PORT} on mode: ${process.env.NODE_ENV}`.blue.bold
   );
+});
+
+// Handle unhandled promise rejcetions
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // close server & exit process
+  server.close(() => process.exit(1));
 });
