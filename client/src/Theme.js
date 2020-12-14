03009eb0606ba4bdc858ae04ad409bf2f67b2178
kluzko <kluzniakkuba@gmail.com>
2020-12-14 22:28:19 +0100

    Added media query

--- client/src/Theme.js
+++ client/src/Theme.js
@@ -1,30 +1,27 @@
-import React from 'react'
-import {ThemeProvider} from 'styled-components'
+import React from "react";
+import { ThemeProvider } from "styled-components";
 // theme for website
 
 const breakPoints = {
-  mobile:'575.98px',
-  tablet:'767.98px',
-}
+  mobile: "599.98px",
+  tablet: "767.98px",
+};
 
 const theme = {
-  colors:{
-    background:'#E5E5E5',
-    primary:'#007CB3',
-    primaryHover:'#0039b3',
+  colors: {
+    background: "#E5E5E5",
+    primary: "#007CB3",
+    primaryHover: "#0039b3",
   },
-  device:{
-    phoneMax:`(max-width: ${breakPoints.mobile})`,
-    phoneMin:`(min-width: ${breakPoints.mobile})`,
-    tabletMax:`(max-wdith: ${breakPoints.tablet})`
-  }
-  
-}
+  device: {
+    phoneMax: `(max-width: ${breakPoints.mobile})`,
+    phoneMin: `(min-width: ${breakPoints.mobile})`,
+    tabletMax: `(max-wdith: ${breakPoints.tablet})`,
+  },
+};
 
-const Theme = ({children}) => (
-  <ThemeProvider theme={theme}>
-    {children}
-  </ThemeProvider>
-)
+const Theme = ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
 
 export default Theme;
