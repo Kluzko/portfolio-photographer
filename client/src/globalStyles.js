de21d9ef13b625e6fa8f75ec60eb73f64b1e1f35
kluzko <kluzniakkuba@gmail.com>
2020-12-14 22:27:51 +0100

    added font to public folder

--- client/src/globalStyles.js
+++ client/src/globalStyles.js
@@ -1,18 +1,15 @@
-import {createGlobalStyle} from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
+const GlobalStyle = createGlobalStyle`
 
- const GlobalStyle = createGlobalStyle`
-
-  @import 
-  url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,100&display=swap');
 
   body, div, dl, dt, dd, ul, ol, li, h1, h2, h3, h4, h5, h6, pre, 
 form, fieldset, input, p, blockquote, table, th, td, embed, object {
 	padding: 0;
 	margin: 0; 
   font-family: 'Lato', sans-serif;
-  color: ${props => props.theme.colors.primary};
-  background: ${props => props.theme.colors.background}
+  color: ${(props) => props.theme.colors.primary};
+  background: ${(props) => props.theme.colors.background}
 	}
  
   body{
@@ -33,7 +30,7 @@ h1, h2, h3, h4, h5, h6, strong, th, var {
 	font-weight: normal;
 	font-style: normal;
 	}
-ul {
+li {
 	list-style: none;
 	}
 
@@ -43,5 +40,5 @@ a, ins {
   color:inherit;
 	}
 
-`
+`;
 export default GlobalStyle;
