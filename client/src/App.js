db7ec16647ca80fc8e8c9e8c25b44839a8958dda
kluzko <kluzniakkuba@gmail.com>
2020-12-14 23:31:29 +0100

    add new route

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import Theme from './Theme'
-
-import GlobalStyle from './globalStyles';
-
-
+import React from "react";
+import Theme from "./Theme";
+import GlobalStyle from "./globalStyles";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 // Components
-import Navbar from './components/navbar/Navbar'
+import Navbar from "./components/Navbar/Navbar.js";
+import Home from "./components/Home/Home";
+import AddAlbum from "./components/Album/AddAlbum";
+import Albums from "./components/Album/Albums";
+
 function App() {
   return (
-    <Theme>
-      <>
-        <GlobalStyle/>
-        <Navbar/>
-      </>
-    </Theme>
+    <Router>
+      <Theme>
+        <GlobalStyle />
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route path="/addAlbum" component={AddAlbum} />
+        <Route path="/albums" component={Albums} />
+      </Theme>
+    </Router>
   );
 }
 
