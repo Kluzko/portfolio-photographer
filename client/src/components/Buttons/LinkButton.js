243f92ccbe0a42c66096712b82e085e674467003
kluzko <kluzniakkuba@gmail.com>
2021-01-22 19:50:07 +0100

    Style fixing

--- client/src/components/Buttons/LinkButton.js
+++ client/src/components/Buttons/LinkButton.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 import { StyledLinkButton } from "./styles";
 
-const LinkButton = ({ link, text }) => (
-  <StyledLinkButton>
-    <Link to={`/${link}`}>{text}</Link>
-  </StyledLinkButton>
-);
+const LinkButton = ({ link, text }) => {
+  let history = useHistory();
+  const PushRoute = () => {
+    history.push(`/${link}`);
+  };
+  return <StyledLinkButton onClick={PushRoute}>{text}</StyledLinkButton>;
+};
 
 export default LinkButton;
--- client/src/components/Footer/style.js
+++ client/src/components/Footer/style.js
@@ -4,9 +4,10 @@ export const FooterWrapper = styled.footer`
   max-height: 8rem;
   background: ${({ theme }) => theme.colors.primary};
 
-  height: 7rem;
+  height: 5rem;
   display: flex;
   align-items: center;
+  position: block;
   justify-content: space-between;
   padding-left: 3rem;
   padding-right: 3rem;
