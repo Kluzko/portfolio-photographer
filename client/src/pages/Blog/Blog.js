52a27195de6bea10fec922feb1b2e639468945fa
kluzko <kluzniakkuba@gmail.com>
2021-02-14 15:40:55 +0100

    pagiantion for articles

--- client/src/pages/Blog/Blog.js
+++ client/src/pages/Blog/Blog.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Loader from "../../components/Loader";
 import ErrorMessage from "../../components/ErrorMessage";
 import { Wrapper } from "../../components/Wrappers";
 import { apiStates, useApi } from "../../hooks/useApi";
 import { ArticleCard } from "../../components/Card";
+import { StyledPagiantion } from "../../components/Dashboard/style";
+import { showVisiblePages } from "../../utils/showVisblePages";
 
 const Blog = () => {
+  // Pagination
+  const [postsPerPage, setPostPerPage] = useState(5);
+  const [currentPage, setCurrentPage] = useState(1);
+  // To give active class current page
+  const [active, setActive] = useState(currentPage - 1);
+
   const {
     data: { state, data, error },
     setRefetch,
@@ -15,17 +23,30 @@ const Blog = () => {
     case apiStates.ERROR:
       return <ErrorMessage>{error || "General error"}</ErrorMessage>;
     case apiStates.SUCCESS:
+      // articles data
       const articles = data.data;
+      // Total of  pages
+      const totalPages = Math.ceil(data.count / postsPerPage);
+      // array of visible pages
+      const visiblePage = showVisiblePages(currentPage, totalPages);
+
+      //get current posts
+      const indexOfLastArticle = currentPage * postsPerPage;
+      const indexOfFirstArticle = indexOfLastArticle - postsPerPage;
+      const currentArticle = articles.slice(
+        indexOfFirstArticle,
+        indexOfLastArticle
+      );
 
       return (
         <Wrapper>
           <div
             style={{
-              marginTop: "20%",
+              marginTop: "10%",
             }}
           >
             {articles.length > 0 ? (
-              articles.map((article) => (
+              currentArticle.map((article) => (
                 <ArticleCard
                   key={article._id}
                   slug={article.slug}
@@ -36,6 +57,31 @@ const Blog = () => {
             ) : (
               <p>No articles</p>
             )}
+          </div>
+          <div
+            style={{
+              marginBottom: "5%",
+              marginTop: "5%",
+              display: "flex",
+            }}
+          >
+            {articles.length > 0 &&
+              visiblePage.map((page, i) => {
+                console.log(i);
+                function setPageAndActive() {
+                  setActive(page - 1);
+                  setCurrentPage(page);
+                }
+                return (
+                  <StyledPagiantion
+                    key={page}
+                    onClick={setPageAndActive}
+                    className={active === i ? "active" : ""}
+                  >
+                    <p>{page}</p>
+                  </StyledPagiantion>
+                );
+              })}
           </div>
         </Wrapper>
       );
